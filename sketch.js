e7213509ff2760342b40350f0e9e8b8164249d1a
YusufGunderwala <78022249+YusufGunderwala@users.noreply.github.com>
2021-03-24 13:03:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,8 +9,8 @@ function setup() {
   box2=createSprite(100,100,150,50);
   box2.shapeColor="cyan";
 
-  box2.debug=true;
-  box1.debug=true;
+ // box2.debug=true;
+ // box1.debug=true;
 }
 
 function draw() {
@@ -19,19 +19,15 @@ function draw() {
   box2.x=World.mouseX;
   box2.y=World.mouseY;
 
-  if(box1.x-box2.x <= (box1.width + box2.width)/2
-  && box2.x - box1.x <= (box1.width + box2.width)/2
-  && box1.y-box2.y <= (box1.height + box2.height)/2
-  && box2.y - box1.y <= (box1.height + box2.height)/2)
-  {
-    box1.shapeColor="green";
-    box2.shapeColor="green";
+  if(touch(box1,box2)){
+    box1.x=random(1,800);
+    box1.y=random(1,800);
+    box1.shapeColor="purple";
   }
-  else{
-    box1.shapeColor="red";
-    box2.shapeColor="cyan";
-  }
+ 
+ 
 
 
   drawSprites();
 }
+
